86d354523b9dca62bbdc49336f2afbb69f2f6f26
CTSdian <wangdian@unc.edu>
2024-09-18 19:57:17 -0400

    Add files via upload

---
+++ assignment_01/hello_world.py
@@ -0,0 +1,25 @@
+# print on one line
+print("this is one line print")
+
+# print on many lines
+print(
+'''
+<p>this is a line</p>
+
+
+<p>this is another line</p>
+'''
+)
+
+print('(this is a test string for "quotes".)')
+
+#test for f-string
+a='test'
+b='f-string'
+print(f'this a {a} for {b}.')
+
+#test for end
+print('this is a test for end',end='.\n')
+
+#test for sep
+print('01','07','2002',sep='-')
---
+++ assignment_01/test.py
@@ -0,0 +1,5 @@
+a=int(input('please input figure a:'))
+b=int(input('please input figure b:'))
+c=int(input('please input figure c:'))
+
+print(f"(a*b)^2-c={(a*b)**2-c}")
